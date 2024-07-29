39bf0116db4aac9b04199316b05456a453cddc17
Faluke2 <119283904+Faluke2@users.noreply.github.com>
2024-07-29 19:50:24 +0700

    upload platform game

---
+++ MyGame/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1722257290,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tilemap.png",
+		"images/player-sheet0.png",
+		"images/hiddenwall-sheet0.png",
+		"images/star-sheet0.png",
+		"images/spike-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/menubg-sheet0.png",
+		"images/playbutton-sheet0.png",
+		"images/tilemap2.png",
+		"images/home-sheet0.png",
+		"images/replay-sheet0.png",
+		"media/jump.m4a",
+		"media/jump.ogg",
+		"media/melodyloops-the-first-light.m4a",
+		"media/melodyloops-the-first-light.ogg",
+		"media/coin8-103286.m4a",
+		"media/coin8-103286.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
